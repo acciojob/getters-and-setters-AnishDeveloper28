30d68fa009ae3c4c45649a313de28a4e92f94999
Anish Jain <anishsinghvi4@gmail.com>
2025-02-11 12:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ class Person {
         this._age = age;
     }
 
-    // Getter for name
     get name(): string {
         return this._name;
-    }
-
-    // Setter for age
-    set age(age: number) {
+	}
+    
+	set age(age: number) {
         this._age = age;
     }
 
@@ -24,33 +22,14 @@ class Person {
     }
 }
 
-// Student Class
 class Student extends Person {
     study(): void {
         console.log(`${this.name} is studying`);
     }
 }
 
-// Teacher Class
 class Teacher extends Person {
     teach(): void {
         console.log(`${this.name} is teaching`);
     }
 }
-
-// Example Code
-
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
